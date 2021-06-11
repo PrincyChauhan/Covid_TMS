01d426c94649cc7b11ec90983c96ef80ab83314a
PrincyChauhan <chauhanprincee7@gmail.com>
2021-06-11 10:57:59 +0530

    Done

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; PC 2021</span>
+                    </div>
+                </div>
+            </footer>
+<!-- End of Footer -->
